fef3d13048f6f4563b8f2013bc8d1534f5d7c964
Vincent Abesamis <vincentabesamis03@gmail.com>
2020-06-23 19:12:59 +0800

    Added the NamesProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
 import './assets/style/index.css'
+import { NamesProvider } from './context/NamesContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NamesProvider>
+      <App />
+    </NamesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
