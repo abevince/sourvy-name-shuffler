32dd974aede026258af7819ff52a3047d01d72f5
Vincent Abesamis <vincentabesamis03@gmail.com>
2020-06-23 18:04:17 +0800

    Added shuffling status and styled the button

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ const names = [
 ]
 const App: React.FC = () => {
   const [name, setName] = useState('Shuffling!')
+  const [isShuffling, setIsShuffling] = useState<boolean>(false)
 
   // useEffect(() => {
   //   const timeOut = setInterval(() => {
@@ -36,12 +37,14 @@ const App: React.FC = () => {
   // }, [])
 
   const loopNames = React.useCallback(async () => {
+    setIsShuffling(true)
     for (let i = 0; i < names.length; i++) {
       await promisifyTimeout(() => {
         setName(names[i])
       }, 200)
       // clearTimeout(timeoutId)
     }
+    setIsShuffling(false)
   }, [])
 
   return (
@@ -49,11 +52,24 @@ const App: React.FC = () => {
       <div className="w-screen h-64 px-4 text-center">
         <h1 className="text-5xl truncate">{name}</h1>
       </div>
+
       <button
+        type="button"
         onClick={loopNames}
-        className="px-5 py-2 text-white bg-gray-800 rounded-lg"
+        disabled={isShuffling}
+        className={`inline-flex items-center mx-auto px-5 py-2 border border-transparent text-lg leading-6 font-medium rounded-md text-white focus:outline-none transition transform ease-in-out duration-150 mt-6 bg-blue-600 hover:bg-blue-500 focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 ${
+          isShuffling && 'scale-0'
+        }`}
       >
-        Start!
+        Shuffle!
+        <svg
+          className="ml-2 -mr-0.5 h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M6.59 12.83L4.4 15c-.58.58-1.59 1-2.4 1H0v-2h2c.29 0 .8-.2 1-.41l2.17-2.18 1.42 1.42zM16 4V1l4 4-4 4V6h-2c-.29 0-.8.2-1 .41l-2.17 2.18L9.4 7.17 11.6 5c.58-.58 1.59-1 2.41-1h2zm0 10v-3l4 4-4 4v-3h-2c-.82 0-1.83-.42-2.41-1l-8.6-8.59C2.8 6.21 2.3 6 2 6H0V4h2c.82 0 1.83.42 2.41 1l8.6 8.59c.2.2.7.41.99.41h2z" />
+        </svg>
       </button>
     </div>
   )
