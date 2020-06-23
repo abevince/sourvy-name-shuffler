d3e9516d38981fb1cabfa5f3536e83513b85d320
Vincent Abesamis <vincentabesamis03@gmail.com>
2020-06-23 15:21:34 +0800

    Add functionality to App component. Todo - Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+function promisifyTimeout(
+  fn: () => void,
+  timeout: number
+): Promise<NodeJS.Timeout> {
+  return new Promise((resolve) => {
+    const timeOutId = setTimeout(() => {
+      fn()
+      resolve(timeOutId)
+    }, timeout)
+  })
+}
+const names = [
+  'Anie',
+  'Vince',
+  'Jerry',
+  'Alfred',
+  'Eugen',
+  'Jerimiah',
+  'Jericho',
+  'Goku',
+]
 const App: React.FC = () => {
-  return <div className="text-2xl">Hello World!</div>
+  const [name, setName] = useState('Shuffling!')
+
+  // useEffect(() => {
+  //   const timeOut = setInterval(() => {
+  //     setName((prevName) => prevName + 1)
+  //   }, 1000)
+  //   return () => clearInterval(timeOut)
+  // }, [])
+
+  const loopNames = React.useCallback(async () => {
+    for (let i = 0; i < names.length; i++) {
+      await promisifyTimeout(() => {
+        setName(names[i])
+      }, 100)
+      // clearTimeout(timeoutId)
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen text-xl">
+      <h1 className="text-6xl">{name}</h1>
+
+      <button
+        onClick={loopNames}
+        className="px-4 py-2 text-white bg-gray-800 rounded"
+      >
+        Start!
+      </button>
+    </div>
+  )
 }
 
 export default App
