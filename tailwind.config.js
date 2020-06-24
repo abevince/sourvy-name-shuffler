fc37f53eec84d7a3f3d6968ebef7af33684ae6b2
Vincent Abesamis <vincentabesamis03@gmail.com>
2020-06-24 09:21:32 +0800

    Added the cool-gray color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'cool-gray': {
+          50: '#fbfdfe',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cfd8e3',
+          400: '#97a6ba',
+          500: '#64748b',
+          600: '#475569',
+          700: '#364152',
+          800: '#27303f',
+          900: '#1a202e',
+        },
+      },
     },
   },
   variants: {},
